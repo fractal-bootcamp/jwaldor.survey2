613aecc17be384386395aa7d97d67205934c6ebd
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-08-07 18:37:05 -0400

    some UI + bug fixes

--- jwaldor.survey2/src/routes/root.tsx
+++ jwaldor.survey2/src/routes/root.tsx
@@ -4,38 +4,6 @@ import { Outlet } from "react-router-dom";
 
 type Survey = { id: string; title: string };
 
-function SurveyList() {
-  const [surveys, setSurveys] = useState([]);
-  useEffect(() => {
-    console.log("here");
-    axios.get("http://localhost:3000/view-surveys").then((response) => {
-      setSurveys(response.data.survey);
-      console.log(response.data.survey);
-    });
-  }, []);
-
-  //   console.log(surveylist);
-
-  return (
-    <>
-      {surveys.map((survey: Survey) => (
-        <ul>
-          <li key={survey.id}>
-            <a href={`complete-survey/${survey.id}`}>{survey.title}</a>
-          </li>
-        </ul>
-      ))}
-      {surveys.map((survey: Survey) => (
-        <ul>
-          <li key={survey.id}>
-            <a href={`view-results/${survey.id}`}>{survey.title}</a>
-          </li>
-        </ul>
-      ))}
-    </>
-  );
-}
-
 export default function Root() {
   const [surveys, setSurveys] = useState([]);
   useEffect(() => {
@@ -49,23 +17,17 @@ export default function Root() {
     <>
       <div id="sidebar">
         <Outlet />–<h1>Happy Surveys!</h1>
-        <a href="create-survey">Create a survey!</a>
+        <div className="bg-slate-200 w-12 h-10 rounded-lg">
+          <a href="create-survey">Create</a>
+        </div>
         <h1>Take a survey?</h1>
         {/* <SurveyList /> */}
         {surveys.map((survey: Survey) => (
-          <ul>
-            <li key={survey.id}>
-              <a href={`complete-survey/${survey.id}`}>{survey.title}</a>
-            </li>
-          </ul>
-        ))}
-        <h1>View survey results!</h1>
-        {surveys.map((survey: Survey) => (
-          <ul>
-            <li key={survey.id}>
-              <a href={`view-results/${survey.id}`}>{survey.title}</a>
-            </li>
-          </ul>
+          <div key={survey.id}>
+            {survey.title} <span> </span>
+            <a href={`complete-survey/${survey.id}`}>take</a> <span> </span>
+            <a href={`view-results/${survey.id}`}>results</a>
+          </div>
         ))}
       </div>
       <div id="detail"></div>
